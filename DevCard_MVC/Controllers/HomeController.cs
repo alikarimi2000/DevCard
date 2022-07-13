fe2aa1a150b2874607ce22a39ffc6fc3eb4c484e
alikarimiDotnet <karimyai46@gmail.com>
2022-07-13 12:38:18 +0430

    add mvc

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -23,7 +23,10 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-     
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
