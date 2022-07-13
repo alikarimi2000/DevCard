ce60971360254f7776d2941f474ffc10c389e010
alikarimiDotnet <karimyai46@gmail.com>
2022-07-13 22:12:13 +0430

    add controller

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
