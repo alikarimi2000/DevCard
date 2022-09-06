c64e9779d064afc11a4f1c8aeaec03b25a36f398
alikarimiDotnet <karimyai46@gmail.com>
2022-09-06 01:13:02 -0700

    public bool get set

--- DevCard_MVC/Models/ErrorViewModel.cs
+++ DevCard_MVC/Models/ErrorViewModel.cs
@@ -7,5 +7,6 @@ namespace DevCard_MVC.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool a{get;set;}
     }
 }
